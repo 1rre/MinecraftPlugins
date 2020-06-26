da859f5f86b2ee543ab1dff4bd7cc8256bf51aa1
tjm1518 <54404449+tjm1518@users.noreply.github.com>
2020-06-26 23:10:46 +0100

    Update persistentdata.scala

--- redstone/src/persistentdata.scala
+++ redstone/src/persistentdata.scala
@@ -3,9 +3,9 @@ package re._1r.spigot.persistentcircuits
 import org.bukkit.persistence._
 import scala.collection.Map
 
-class ComparatorIc extends PersistentDataType[(Int, Int) => Int, (Int, Int) => Int]{
-  override def getPrimitiveType: Class[(Int, Int) => Int] = {
-    ((x:Int, y:Int) => 0).getClass.asInstanceOf[Class[(Int, Int) => Int]]
+class ComparatorIc extends PersistentDataType[Array[Byte], (Int, Int) => Int]{
+  override def getPrimitiveType: Class[Array[Byte]] = {
+    (Array[Byte].getClass.asInstanceOf[Class[Array[Byte]]]
   }
   override def getComplexType: Class[(Int, Int) => Int] = {
     ((x:Int, y:Int) => 0).getClass.asInstanceOf[Class[(Int, Int) => Int]]
@@ -13,9 +13,26 @@ class ComparatorIc extends PersistentDataType[(Int, Int) => Int, (Int, Int) => I
   override def toPrimitive(
     complex: (Int, Int) => Int,
     context: PersistentDataAdapterContext
-  ): (Int, Int) => Int = complex
+  ): Array[Byte] = {
+    import java.io.{ByteArrayOutputStream, ObjectOutputStream}
+    val baos = new ByteArrayOutputStream
+    val writer = new ObjectOutputStream(baos)
+    writer.writeObject(complex)
+    writer.close
+    val rtn = os.toByteArray
+    os.close
+    rtn
+  }
   override def fromPrimitive(
-    primitive: (Int, Int) => Int,
+    primitive: Array[Byte],
     context: PersistentDataAdapterContext
-  ): (Int, Int) => Int =  primitive
+  ): (Int, Int) => Int = {
+    import java.io.{ByteArrayInputStream, ObjectInputStream}
+    val bais = new ByteArrayInputStream(primitive)
+    val reader = new ObjectInputStream(bais)
+    val rtn = reader.readObject.asInstanceOf[Int => Int]
+    reader.close
+    bais.close
+    rtn
+  }
 }
